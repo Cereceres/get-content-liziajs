faa6083a5ffedb8fa595a426e27969c025465a1d
cereceres <jesus.edelcereceres@gmail.com>
2017-12-11 17:49:42 -0600

    The test are fixed

--- test/test.js
+++ test/test.js
@@ -1,13 +1,11 @@
-'use strict'
-const getTree = require('../index')
-var assert = require('assert')
+const getTree = require('../index');
+const assert = require('assert');
 describe('test to getTree', () => {
-  it('should get a object', function () {
-    let res = getTree('../')
-    console.log('res',  JSON.stringify(res, null, 4))
-    assert(typeof res === 'object')
-    assert(Array.isArray(res.test.test1.test2.test3.files))
-    assert(typeof getTree.includeHidden === 'function')
-    assert(typeof getTree.excludeHidden === 'function')
-  })
-})
+    it('should get a object', () => {
+        const res = getTree('../');
+        assert(typeof res === 'object');
+        assert(Array.isArray(res.test.test1.test2.test3.files));
+        assert(typeof getTree.includeHidden === 'function');
+        assert(typeof getTree.excludeHidden === 'function');
+    });
+});
