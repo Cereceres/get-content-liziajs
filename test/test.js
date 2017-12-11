934b8c73910af0b07f461f25c6ee0884b8ef0a59
cereceres <jesus.edelcereceres@gmail.com>
2017-12-11 17:53:44 -0600

    test are fixed

--- test/test.js
+++ test/test.js
@@ -5,6 +5,7 @@ describe('test to getTree', () => {
         const res = getTree('../');
         assert(typeof res === 'object');
         assert(Array.isArray(res.test.test1.test2.test3.files));
+        assert(res.test.test1.test2.test3.files[0]['index.js']);
         assert(typeof getTree.includeHidden === 'function');
         assert(typeof getTree.excludeHidden === 'function');
     });
